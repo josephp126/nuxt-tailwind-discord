06e9bd70b110585a72f37158d134caa75f419ae9
NeTT <dentfooly@gmail.com>
2021-08-22 22:46:20 +0530

    copypaste from svelte-rewrite

---
+++ pages/privacy/index.vue
@@ -0,0 +1,242 @@
+
+<template>
+  <div>
+    <div id="headerWrapper">
+      <div class="block">
+        <div class="flex-col mx-auto items-center">
+          <div class="mx-auto self-center">
+            <a href="/" class="w-48">
+              <img
+                src="/img/90x90.png"
+                class="w-20 p-2 mx-auto object-cover"
+                alt="logo"
+              />
+            </a>
+          </div>
+          <div class="text-center p-2">
+            <p class="text-4xl font-semibold">Privacy Policy</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="privacyWrapper" class="">
+      <div class="privacy-container">
+        <div class="privacyContent">
+          <div class="flex justify-between privacy-head">
+            <div class="privacyHeader">
+              <h1>Terms of Service</h1>
+              <p>Updated Mar 12, 2020</p>
+            </div>
+
+            <div class="get-privacy-terms align-self-center">
+              <button
+                class="
+                  bg-discortics-200
+                  flex
+                  rounded-md
+                  block
+                  px-4
+                  py-2
+                  transition
+                  duration-500
+                  ease-in-out
+                  transform
+                  hover:-translate-y-1
+                "
+                onClick="window.print()"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-printer"
+                >
+                  <polyline points="6 9 6 2 18 2 18 9"></polyline>
+                  <path
+                    d="M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2"
+                  ></path>
+                  <rect x="6" y="14" width="12" height="8"></rect>
+                </svg>
+                <span class="px-2">Print</span>
+              </button>
+            </div>
+          </div>
+
+          <div class="privacy-content-container">
+            <section>
+              <h5>Please read our policy carefully</h5>
+              <p>
+                Welcome to discortics.com, a service provided by Discortics™.
+                Please read on to learn the rules and restrictions that govern
+                your use of our website(s), products, services, proprietary
+                bots, and other applications (the “Services”). If you have any
+                questions, comments, or concerns regarding these terms or the
+                Services, please contact us in the support server.
+              </p>
+
+              <p>
+                These Terms of Use (the “Terms”) are a binding contract between
+                you and DISCORTICS.GA, LLC. (“Discortics”, “we” and “us”). Your
+                use of the Services in any way means that you agree to all of
+                these Terms, and these Terms will remain in effect while you use
+                the Services. These Terms include the provisions in this
+                document as well as those in the Privacy Policy. Your use of or
+                participation in certain Services may also be subject to
+                additional policies, rules and/or conditions (“Additional
+                Terms”), which are incorporated herein by reference, and you
+                understand and agree that by using or participating in any such
+                Services, you agree to also comply with these Additional Terms.
+              </p>
+
+              <p>
+                We reserve the right to change this without notifying our users.
+              </p>
+            </section>
+
+            <h5 class="policy-info-ques">
+              What personal data we collect and why we collect it
+            </h5>
+
+            <section>
+              <h5>Media</h5>
+
+              <p>
+                If you upload images to the website, you should avoid uploading
+                images with embedded location data (EXIF GPS) included. Visitors
+                to the website can download and extract any location data from
+                images on the website.
+              </p>
+            </section>
+
+            <section>
+              <h5>Cookies</h5>
+
+              <p>
+                If you have an account and you log in to this site, we will set
+                a temporary cookie to determine if your browser accepts cookies.
+                This cookie contains no personal data and is discarded when you
+                close your browser.
+              </p>
+
+              <p>
+                When you log in, we will also set up several cookies to save
+                your login information and your screen display choices. Login
+                cookies last for days. If you log out of your account, the login
+                cookies will be removed.
+              </p>
+            </section>
+
+            <section>
+              <h5>Embedded content from other websites.</h5>
+
+              <p>
+                Articles on this site & the bot may include embedded content
+                (e.g. videos, images, articles, etc.). We do not own the content
+                displayed.
+              </p>
+
+              <p>
+                We use <a href="https://arc.io">arc.io</a> to monetize our
+                dashboard. Arc does not collect your data, you may visit their
+                website & review their policy. You will see a blue circle in the
+                left-bottom corner. In order to opt out from Arc, select Opt out
+                from the menu which pops up on click.
+              </p>
+            </section>
+
+            <section>
+              <h5>Device Fingerprint</h5>
+
+              <p>
+                THIS IS ONLY APPLICABLE TO SERVERS & ITS USERS WHICH UTILISES
+                OUR VERIFICATION SYSTEM. Discortics stores your device
+                fingerprints such as IP Address inorder to provide secure & safe
+                enviornment for the users. The data is stored using SHA 512 with
+                RSA Encryption in our highly secure database. The access to the
+                database is restricted to the owner of Discortics™.
+              </p>
+            </section>
+
+            <section>
+              <h5>User Tracking</h5>
+
+              <p>
+                In order to provide statistical data to the server admins, user
+                activities such as message count, time in VC, invites etc are
+                tracked server-wise.
+              </p>
+            </section>
+
+            <h5 class="policy-info-ques">Who gets this data?</h5>
+
+            <section>
+              <h5>We do not sell your data</h5>
+
+              <p>
+                Your data is safe with us and isn't being shared with any third
+                parties.
+              </p>
+            </section>
+
+            <section>
+              <h5>The Owner</h5>
+
+              <p>
+                Your data is stored in our highly secure database. The access to
+                the database is restricted to the owner of Discortics™.
+              </p>
+            </section>
+
+            <h5 class="policy-info-ques">Questions & Concerns</h5>
+
+            <section>
+              <h5>Support Server</h5>
+
+              <p>
+                If you are concerned/suspicious about any of the above given
+                statements/data , you can enquire about it in
+                <a href="https://discord.gg/buHBCtE">The Support Server</a>
+              </p>
+            </section>
+
+            <h5 class="policy-info-ques">Remove Your Data</h5>
+
+            <section>
+              <h5>Data Retention</h5>
+
+              <p>
+                Even if the bot is removed from a server, the settings
+                configured for the bot still exists for 30 days. If you want to
+                have it removed, you can have the rights to request the devs to
+                do so. Once deleted, its not possible to recover it back.
+              </p>
+
+              <p>
+                Additionally, if you would like to request for removal, contact
+                <a href="https://discord.gg/buHBCtE">The Support Server</a>
+              </p>
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'privacy',
+  head() {
+    return {
+      title: 'Privacy Policy',
+    }
+  },
+}
+</script>
+<style scoped src = '@/static/css/privacy.css' />
