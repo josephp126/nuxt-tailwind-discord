5698f8a8ef2ea8b16fb8e64c840cfc5aa81f3802
NeTT <dentfooly@gmail.com>
2021-08-23 16:49:33 +0530

    create Features.js

---
+++ data/Features.js
@@ -0,0 +1,50 @@
+export default [
+    {
+        name: "Applications",
+        description: "Setup & Manage Staff Applications in your server",
+    },
+    {
+        name: "Giveaways",
+        description: "Host giveaways with various Requirements & Customised Embeds",
+    },
+    {
+        name: "Levels",
+        description: "Highly customisable Leveling system with UI Rank Cards & Role Rewards",
+    },
+    {
+        name: "Verification",
+        description: "Advanced Protection from Fake Accounts & Proxy/VPN Users",
+    },
+    {
+        name: "Tracking",
+        description: "Track Joins, Leaves, Invites, Voice Time, Behaviour, Messages & Last Active Time",
+    },
+    {
+        name: "Tickets",
+        description: "Sophisticated Reaction based Ticket System with Live Dashboard*",
+    },
+    {
+        name: "Artificial Intelligence",
+        description: "Latest Conversational Artificial Intelligence to engage your members",
+    },
+    {
+        name: "Channel Management",
+        description: "Create & Manage Suggestion, Confession and Starboard channels",
+    },
+    {
+        name: "Global AFK",
+        description: "Global AFK System to save you from setting AFK in multiple servers",
+    },
+    {
+        name: "Economy & Games",
+        description: "Server-wise economy & engaging games such as Among Us",
+    },
+    {
+        name: "Notification",
+        description: "Recieve notification from Youtube, Twitch, & Many More*",
+    },
+    {
+        name: "Moderation",
+        description: "Powerful Moderation Commands and Anti-spam Filters",
+    }
+]
