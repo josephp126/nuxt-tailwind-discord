2e3c9e4b26bcb0dcb4f2d917d385967492d8ce42
NeTT <dentfooly@gmail.com>
2021-08-23 16:50:06 +0530

    pass title to navbar

---
+++ layouts/dashboard.vue
@@ -0,0 +1,9 @@
+
+<template>
+  <div class="flex flex-col min-h-screen font-quicksand tracking-wide bg-discortics-100 text-white">
+    <LayoutSidebar :title="$nuxt.$route.path.split('/').reverse()[0]" />
+    <main class="flex-1 w-full max-w-9xl p-4 mx-auto md:px-8 md:py-16 mt-5">
+      <Nuxt />
+    </main>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -17,10 +17,10 @@ export default {
 
 <template>
   <div class="flex flex-col min-h-screen font-quicksand tracking-wide bg-discortics-100 text-white">
-    <Header :title="$nuxt.$route.name" />
+    <LayoutHeader :title="$nuxt.$route.path.split('/').reverse()[0]" />
     <main class="flex-1 w-full max-w-9xl p-4 mx-auto md:px-8 md:py-16 mt-5">
       <Nuxt />
     </main>
-    <Footer />
+    <LayoutFooter />
   </div>
 </template>
