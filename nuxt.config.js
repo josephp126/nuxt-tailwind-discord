79c9d05dc7ff6065f30392d7132d8e3ef0cf6215
NeTT <dentfooly@gmail.com>
2021-08-28 18:05:07 +0530

    config toast

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,6 @@
+import Constants from './data/Constants'
+import Secret from './data/Secret'
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -18,11 +21,20 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@sweetalert2/theme-dark/dark.css'
   ],
+
+  // Loading Bar
+  loading: {
+    color: '#25d5e4',
+    height: '2px'
+  },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/vue-tooltip.js'
+    '~/plugins/vue-tooltip.js',
+    '~/plugins/api.js',
+    '~/plugins/swal.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -40,12 +52,99 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/proxy',
+    '@nuxtjs/auth-next',
+    '@nuxtjs/toast',
   ],
+
+  // Proxy
+  proxy: {
+    '/api': { target: 'http://api.discortics.com', changeOrigin: true, pathRewrite: {'/api/': '/'} },
+  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
+  // Toast module configuration
+  toast: {
+    position: 'top-center',
+    register: [ // Register custom toasts
+      {
+        name: 'invite',
+        message: 'Invite bot',
+        options: {
+          className: 'bg-discortics-dashboard',
+          type: 'info',
+          duration: 10000,
+          action : [
+            {
+                text : 'Admin Permissions',
+                push : {
+                  name : Constants.Invite_Admin,
+                  dontClose : true
+                 }
+            },
+            {
+                text : 'Normal Permissions',
+                push : {
+                  name : Constants.Invite_Normal,
+                  dontClose : true
+                 }
+            }
+        ]
+        }, 
+      },
+      {
+        name: 'comm-error',
+        message: 'Unable to contact Discortics',
+        options: {
+          type: 'error'
+        }
+      },
+      {
+        name: 'error',
+        message: 'An unknown error occured',
+        options: {
+          type: 'error'
+        }
+      }
+    ]
+  },
+
+  // Auth module configuration
+  auth: {
+    redirect: {
+      login: '/login',
+      logout: '/',
+      callback: '/dashboard',
+      home: '/dashboard'
+    },
+    rewriteRedirects: true,
+    strategies: {
+      discordapp: {
+        scheme: 'oauth2',
+        endpoints: {
+          authorization: 'https://discord.com/api/oauth2/authorize',
+          token: 'https://discord.com/api/oauth2/token',
+          userInfo: 'https://discord.com/api/users/@me'
+        },
+        clientId: Secret.clientID,
+        clientSecret: Secret.clientSecret,
+        codeChallengeMethod: "",
+        responseType: 'token',
+        scope: "identify guilds",
+        autoLogout: "true",
+      }
+    }
+  },
+
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    devMiddleware: {
+      headers: {
+        'Cache-Control': 'no-store',
+        Vary: '*'
+      }
+    }
   }
 }
