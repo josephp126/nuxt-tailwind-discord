6014a75c4d772adb07c3c9ede7e37abeca40e815
Caligo <schattenetcaligo@gmail.com>
2021-12-13 20:08:46 +0530

    fix overflow

--- components/Dash/Language.vue
+++ components/Dash/Language.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="py-1 lg:py-4 px-4">
-      <div class="mx-auto max-w-xl form-card w-full lg:w-112 relative lg:h-64 overflow-hidden">
+      <div class="mx-auto max-w-xl form-card w-full lg:w-112 relative lg:h-64">
         <div class="px-8">
           <div class="gradient-circle w-24 h-24 absolute right-2 top-2" />
           <div class="pt-2">
