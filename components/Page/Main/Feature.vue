3819a8203f04e6bd19473c5c10c8ca920a65cce9
infinitytmbots <69136448+infinitytmbots@users.noreply.github.com>
2021-12-14 22:44:19 +0530

    Completed General>Configurations card styling

--- components/Page/Main/Feature.vue
+++ components/Page/Main/Feature.vue
@@ -1,31 +1,60 @@
 <template>
   <div class="p-4">
     <div
-      class="
+      :class="`
         font-montserrat cursor-default
-        hover:bg-featureHover
+        hover:${ name == 'Premium' ? 'bg-featureHoverPrem' : 'bg-featureHover' }
         hover:bg-purple-900
         border-gray-700 border-2
         transform
         transition-all group
         duration-500
         ease-in-out
-        hover:translate-y-1
+        hover:-translate-y-1
         block
         py-2 px-3
         w-full
         h-48
         lg:w-72
         rounded-xl
-      "
+         ${ name == 'Premium' ? 'hover:text-black' : '' }
+       `"
     >
+    <img
+          v-if="name == 'Premium'"
+          src="/img/featureoverprem.png"
+          class="
+            hidden group-hover:block
+            w-full
+            h-full
+            absolute
+            object-cover
+            inset-0
+            bg-black bg-opacity-10
+            z-20
+          "
+        />
+      <img
+          v-else
+          src="/img/featureover.png"
+          class="
+            hidden group-hover:block
+            w-full
+            h-full
+            absolute
+            object-cover
+            inset-0
+            bg-black bg-opacity-10
+            z-20
+          "
+        />
       <div class="flex flex-col justify-start w-full h-full">
-        <div class="text-xl p-2 font-semibold flex flex-row items-center justify-between w-full">
+        <div class="text-xl p-2 font-quicksand font-semibold flex flex-row items-center justify-between w-full">
           <span class="rounded-full pr-2 text-yellow-500">
             <SVGNewWrapper :name="svg" />
           </span>
           <span class = "w-full">{{ name }}</span>
-          <NuxtLink :to = "route" :class = "`p-3 cursor-pointer rounded-xl text-sm font-medium group-hover:bg-button group-hover:text-white bg-none ${name == 'Premium' ? 'bg-yellow-300 text-black' : 'bg-gray-600'} group-hover:bg-transparent transition duration-500 ease-in-out`">Go</NuxtLink>
+          <NuxtLink :to = "route" :class = "`py-3 px-4 cursor-pointer rounded-xl text-sm font-medium group-hover:${ name == 'Premium' ? 'bg-featureprembbg' : 'bg-button' } group-hover:shadow-xl group-hover:${ name == 'Premium' ? 'text-black' : 'text-white' } bg-none ${/* name == 'Premium' ? 'bg-yellow-300 text-black' : */ 'bg-gray-600'} group-hover:bg-transparent transition duration-500 ease-in-out`">Go</NuxtLink>
         </div>
 
         <p class="text-sm p-2 font-quicksand">
--- tailwind.config.js
+++ tailwind.config.js
@@ -58,6 +58,7 @@ module.exports = {
       backgroundImage: {
         'featureHover': 'linear-gradient(114deg, rgba(79,164,249,0.7) -73%, rgba(242,118,250,0.7) 240%)',
         'featureHover1': 'linear-gradient(113.57deg, #4FA4F9 -74.59%, rgba(242, 118, 250, 0) 249.06%)',
+        'featureHoverPrem': 'linear-gradient(113.57deg, #FFDD86 -74.59%, rgba(250, 166, 118, 0.51) 249.06%)',
         'button': 'linear-gradient(90deg, #1E1A56 -0.59%, rgba(45, 43, 120, 0.0734855) 284.31%, rgba(46, 44, 122, 0) 335.77%)',
         'navCurrent': 'linear-gradient(90deg, #7875FC 0%, #4FA4F9 0.03%, #F586C4 114.81%, rgba(249, 174, 131, 0) 317.33%)',
         'buttonBg': ' linear-gradient(83.08deg, #4FEBF9 7.34%, #3FC1CD 92.07%)',
@@ -80,6 +81,7 @@ module.exports = {
       },
       colors: {
         selectorhighlight: '#5E76C0',
+        featureprembbg: '#FFDD86',
         green4f: "#4FEBF9",
         black24: "#242424",
         gray9d: "#9D9D9D",
